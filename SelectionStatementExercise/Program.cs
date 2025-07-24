3da6571b4b5cd64198ba8b19a9c4d3c90e64e8f6
Kayla Knelle <kaynell3234@gmail.com>
2025-07-23 21:29:52 -0400

    Completed the Random Number Guessing Game.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(
+                "Hello I will let the computer choose a random number, I think my favorite number will\nbe too easy to guess! You Have 5 tries...\nGood Luck!");
+            var r = new Random();
+            var favoriteNumber = r.Next(1, 100);
+            for (int i = 0; i < 5; i++)
+                do
+                {
+
+                    Console.WriteLine("Guess any Number between 1 and 100!");
+                    var userInput = int.Parse(Console.ReadLine());
+
+
+                    if (userInput > 1 && userInput < 100)
+                    {
+
+                        if (userInput < favoriteNumber)
+                        {
+                            Console.WriteLine("Too Low!");
+                        }
+                        else if (userInput > favoriteNumber)
+                        {
+                            Console.WriteLine("Too High!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Woah How Did You Get That?\nGreat Job!!");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid answer.");
+                    }
+
+                } while (favoriteNumber != favoriteNumber);
+
+
+            Console.WriteLine("Ooh too bad try again");
         }
     }
 }
